d9545e4f1a5f8ded2bacc3ab83db30a6a5ea9f71
kishtarn555 <hec_montemoon@hotmail.com>
2024-06-20 21:43:14 -0600

    [gulp] Door generator moved

---
+++ gulpfile.door.js
@@ -0,0 +1,105 @@
+
+function getDoorPermutation(data, facing, upper_block_bit, door_hinge_bit, open_bit) {
+
+    const geometry = {
+      "identifier": upper_block_bit ? "geometry.cc_door_down" : "geometry.cc_door_up",
+      "bone_visibility": {
+        "normal": "!q.block_state('cc:door_hinge_bit') != q.block_state('cc:open_bit')",
+        "back": "q.block_state('cc:door_hinge_bit') != q.block_state('cc:open_bit')"
+      }
+  
+    };
+    const rots = {
+      "north": 0,
+      "east": -90,
+      "west": 90,
+      "south": 180
+    };
+    const rotY = rots[facing];
+    const dY = 90 * (door_hinge_bit ? 1 : -1) * (open_bit ? 1 : 0);
+  
+  
+    const perm = {
+      "condition": `q.block_state('minecraft:cardinal_direction') == '${facing}' && q.block_state('cc:upper_block_bit') == ${upper_block_bit} && q.block_state('cc:door_hinge_bit') == ${door_hinge_bit} && q.block_state('cc:open_bit') == ${open_bit}`,
+      "components": {
+        "minecraft:geometry": geometry,
+        "minecraft:material_instances": {
+          "*": {
+            "texture": upper_block_bit ? data.texture.top : data.texture.bottom,
+            "render_method": "blend"
+          }
+        },
+        "minecraft:transformation": { "rotation": [0, rotY + dY, 0] },
+      }
+    };
+  
+    return perm;
+  }
+  
+  function getDoorJSON(data) {
+  
+    permutations = [
+    ]
+    for (let i = 0; i < 8; i++) {
+      permutations.push(
+        getDoorPermutation(data, "north", (i & 4) != 0, (i & 2) != 0, (i & 1) != 0)
+      );
+      permutations.push(
+        getDoorPermutation(data, "east", (i & 4) != 0, (i & 2) != 0, (i & 1) != 0)
+      );
+      permutations.push(
+        getDoorPermutation(data, "south", (i & 4) != 0, (i & 2) != 0, (i & 1) != 0)
+      );
+      permutations.push(
+        getDoorPermutation(data, "west", (i & 4) != 0, (i & 2) != 0, (i & 1) != 0)
+      );
+    }
+  
+    const object = {
+      "format_version": "1.20.60",
+      "minecraft:block": {
+        "description": {
+          "identifier": data.identifier,
+          "menu_category": {
+            "category": "construction",
+            "group": "itemGroup.name.door",
+            "is_hidden_in_commands": false
+          },
+          "traits": {
+            "minecraft:placement_direction": {
+              "enabled_states": ["minecraft:cardinal_direction"],
+              "y_rotation_offset": 180
+            }
+          },
+          "states": {
+            "cc:upper_block_bit": [false, true],
+            "cc:door_hinge_bit": [false, true],
+            "cc:open_bit": [false, true]
+          }
+        },
+        "components": {
+          // "minecraft:collision_box": {
+          //   "origin": [-8, 0, -8],
+          //   "size": [16, 16, 3]
+          // },
+          "minecraft:collision_box": false,
+          "minecraft:selection_box": {
+            "origin": [-8, 0, -8],
+            "size": [16, 16, 3]
+          },
+          "minecraft:placement_filter": {
+            "conditions": [
+              {
+                "allowed_faces": ["up"]
+              }
+            ]
+          },
+          "tag:cc_door": {}
+        },
+        "permutations": permutations
+      }
+    };
+    return object
+  }
+
+  exports.getDoorJSON = getDoorJSON;
--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,8 @@ const os = require("os");
 const spawn = require("child_process").spawn;
 const sourcemaps = require("gulp-sourcemaps");
 const jsonTransform = require('gulp-json-transform');
+const { getDoorJSON } = require('./gulpfile.door');
+
 
 const worldsFolderName = useMinecraftDedicatedServer ? "worlds" : "minecraftWorlds";
 
@@ -45,108 +47,6 @@ function copy_resource_packs() {
   return gulp.src(["resource_packs/**/*"]).pipe(gulp.dest("build/resource_packs"));
 }
 
-function getDoorPermutation(data, facing, upper_block_bit, door_hinge_bit, open_bit) {
-
-  const geometry = {
-    "identifier": upper_block_bit ? "geometry.cc_door_down" : "geometry.cc_door_up",
-    "bone_visibility": {
-      "normal": "!q.block_state('cc:door_hinge_bit') != q.block_state('cc:open_bit')",
-      "back": "q.block_state('cc:door_hinge_bit') != q.block_state('cc:open_bit')"
-    }
-
-  };
-  const rots = {
-    "north": 0,
-    "east": -90,
-    "west": 90,
-    "south": 180
-  };
-  const rotY = rots[facing];
-  const dY = 90 * (door_hinge_bit ? 1 : -1) * (open_bit ? 1 : 0);
-
-
-  const perm = {
-    "condition": `q.block_state('minecraft:cardinal_direction') == '${facing}' && q.block_state('cc:upper_block_bit') == ${upper_block_bit} && q.block_state('cc:door_hinge_bit') == ${door_hinge_bit} && q.block_state('cc:open_bit') == ${open_bit}`,
-    "components": {
-      "minecraft:geometry": geometry,
-      "minecraft:material_instances": {
-        "*": {
-          "texture": upper_block_bit ? data.texture.top : data.texture.bottom,
-          "render_method": "blend"
-        }
-      },
-      "minecraft:transformation": { "rotation": [0, rotY + dY, 0] },
-    }
-  };
-
-  return perm;
-}
-
-function getDoorJSON(data) {
-
-  permutations = [
-  ]
-  for (let i = 0; i < 8; i++) {
-    permutations.push(
-      getDoorPermutation(data, "north", (i & 4) != 0, (i & 2) != 0, (i & 1) != 0)
-    );
-    permutations.push(
-      getDoorPermutation(data, "east", (i & 4) != 0, (i & 2) != 0, (i & 1) != 0)
-    );
-    permutations.push(
-      getDoorPermutation(data, "south", (i & 4) != 0, (i & 2) != 0, (i & 1) != 0)
-    );
-    permutations.push(
-      getDoorPermutation(data, "west", (i & 4) != 0, (i & 2) != 0, (i & 1) != 0)
-    );
-  }
-
-  const object = {
-    "format_version": "1.20.60",
-    "minecraft:block": {
-      "description": {
-        "identifier": data.identifier,
-        "menu_category": {
-          "category": "construction",
-          "group": "itemGroup.name.door",
-          "is_hidden_in_commands": false
-        },
-        "traits": {
-          "minecraft:placement_direction": {
-            "enabled_states": ["minecraft:cardinal_direction"],
-            "y_rotation_offset": 180
-          }
-        },
-        "states": {
-          "cc:upper_block_bit": [false, true],
-          "cc:door_hinge_bit": [false, true],
-          "cc:open_bit": [false, true]
-        }
-      },
-      "components": {
-        // "minecraft:collision_box": {
-        //   "origin": [-8, 0, -8],
-        //   "size": [16, 16, 3]
-        // },
-        "minecraft:collision_box": false,
-        "minecraft:selection_box": {
-          "origin": [-8, 0, -8],
-          "size": [16, 16, 3]
-        },
-        "minecraft:placement_filter": {
-          "conditions": [
-            {
-              "allowed_faces": ["up"]
-            }
-          ]
-        },
-        "tag:cc_door": {}
-      },
-      "permutations": permutations
-    }
-  };
-  return object
-}
 
 
 function build_doors() {
